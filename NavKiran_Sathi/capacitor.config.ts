4c397029574ffffca50168100a55c05615694c20
Prakhar bhartiya <prakharbhartiya01@gmail.com>
2023-01-05 19:13:16 +0530

    Initial Commit - Ionic + Angular setup

---
+++ NavKiran_Sathi/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NavKiran_Sathi',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
